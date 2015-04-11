5a24a8a3cf5327cd561bc6bd9d2d89cd9166b736
williamyxl <williamyxl@gmail.com>
2015-04-11 16:40:18 -0500

    Revert "500->1000差值函数"
    This reverts commit d93d6ad76b1f8549e9b18479576a15c2662f99d4.

--- interpolation.m
+++
@@ -1,6 +0,0 @@
-function [z1, z2, z3, z4] = interpolation(z, g1, g2, g3, g4, dx , dy)
-    z1 = z + g1 * [-0.5 * dx; -0.5 * dy];
-    z2 = z + g2 * [0.5 * dx; -0.5 * dy];
-    z3 = z + g3 * [-0.5 * dx; 0.5 * dy];
-    z4 = z + g4 * [0.5 * dx; 0.5 * dy];
-end
